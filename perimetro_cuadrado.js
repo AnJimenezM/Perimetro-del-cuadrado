b987a3a539355e2f5a9efef8077d5f7c50d35d1d
AnJimenezM <136032517+AnJimenezM@users.noreply.github.com>
2023-06-24 12:41:01 -0500

    commit

---
+++ perimetro_cuadrado.js
@@ -0,0 +1,9 @@
+
+
+var lado1 = parseFloat(prompt("lado1"));
+var lado2 = parseFloat(prompt("lado2"));
+var lado3 = parseFloat(prompt("lado3"));
+var lado4 = parseFloat(prompt("lado4"));
+perimetro = lado1 + lado2 + lado3 + lado4;
+
+alert(perimetro + "cm");
